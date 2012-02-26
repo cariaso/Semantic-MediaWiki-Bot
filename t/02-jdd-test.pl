ce3c24f574ac200a34e42b364f8349ab2c081a1d
Michael Cariaso <cariaso@gmail.com>
2012-02-26 13:29:11 +0900

    test cleanups

--- t/02-jdd-test.pl
+++ t/02-jdd-test.pl
@@ -5,6 +5,7 @@ use warnings;
 use MediaWiki::Bot;
 use Data::Dumper;
 
+# still uses POST, not GET until this is fixed
 # https://rt.cpan.org/Public/Bug/Display.html?id=75296
 
 my $bot = MediaWiki::Bot->new();
@@ -15,22 +16,24 @@ $bot->set_wiki({
 	       });
 
 
-my $results = $bot->ask({
+my $results1 = $bot->ask({
     query=>"[[Category:Is a genotype]] [[On chromosome::1]]
 |?Allele1
 |?Allele2
+|?On chromosome
 |limit=500",
 			});
 
 
 
-my $results = $bot->askargs({
+my $results2 = $bot->askargs({
     conditions=>"[[Category:Is a genotype]] [[On chromosome::1]]",
-    outs=>['Allele1','Allele2'],
+    outs=>['Allele1','Allele2','On chromosome'],
     #parameters=>'|limit=5',
-    
 			});
-print Dumper $results;
+print Dumper $results1;
+print Dumper $results2;
+
 __END__
 my $i = 0;
 foreach my $key (keys %$results) {
